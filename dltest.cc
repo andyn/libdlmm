722467aae3739ae267aaf20d1def97b35cdee623
Antti Nilakari <antti.nilakari@gmail.com>
2012-11-13 14:15:07 +0200

    Updated test a bit.

--- dltest.cc
+++ dltest.cc
@@ -8,39 +8,47 @@
 int main(void) {
     Dl dl("libuseless.so");
 
-    std::cout << "Testing valid library calls:\n" << std::endl;
+    std::cout << "Testing valid library calls..." << std::endl;
 
-    std::cout << "Variable from library: " << std::endl
-              << *dl.symbol<int>("variable") << std::endl;
-    std::cout << "Function call from library: " << std::endl;
-    std::cout << dl.symbol<int(std::string, int)>("function")("Hello World!", 100) << std::endl;
-    
-    std::cout << "\nTesting invalid library calls, each should throw:\n***" << std::endl;
+    std::cout << "Variable from library should be 42..." << std::endl;
+    if (*dl.symbol<int>("variable") != 42) {
+        std::cout << "FAILED: WAS " << *dl.symbol<int>("variable") << std::endl;
+        return -1;
+    }
+
+    std::cout << "Function call from library should print \"Hello world!\" and increment 100 to 101..." << std::endl;
+    int i = dl.symbol<int(std::string, int)>("function")("Hello World!", 100);
+    if (i != 101) {
+        std::cout << "FAILED: WAS " << i << std::endl;
+        return -1;
+    }
+
+    std::cout << "Testing invalid library calls..." << std::endl;
     
     try {
-        Dl dl2("nonexistant_library");
+        Dl dl2("nonexistent_library");
         std::cout << *dl.symbol<int>("should_not_reach_me");
-        std::cout << "*** TESTING FOR NONEXISTANT LIBRARY FAILED!" << std::endl;
+        std::cout << "*** TEST FAILED: INVALID LIBRARY DID NOT THROW" << std::endl;
+        return -1;
     }
     catch (std::runtime_error e) {
-        std::cout << e.what() << std::endl;
     }
     
     try {
-        std::cout << *dl.symbol<int>("nonexistant_symbol");
-        std::cout << "*** TESTING FOR NONEXISTANT SYMBOL FAILED!" << std::endl;
+        std::cout << *dl.symbol<int>("nonexistent_symbol");
+        std::cout << "*** TEST FAILED: NONEXISTENT SYMBOL DID NOT THROW" << std::endl;
+        return -1;
     }
     catch (std::runtime_error e) {
-        std::cout << e.what() << std::endl;
     }
     
     try {
-        std::cout << *dl.symbol<int>("variable", "nonexistant_version");
-        std::cout << "*** TESTING FOR NONEXISTANT SYMBOL VERSION FAILED!" << std::endl;
+        std::cout << *dl.symbol<int>("variable", "nonexistent_version");
+        std::cout << "*** TEST FAILED: NONEXISTENT VERSION OF SYMBOL DID NOT THROW" << std::endl;
+        return -1;
     }
     catch (std::runtime_error e) {
-        std::cout << e.what() << std::endl;
     }
     
-    std::cout << "\nDone testing." << std::endl;    
+    std::cout << "Done testing." << std::endl;    
 }   
