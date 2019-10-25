6b1eecbeac6b6e75bff10e84a7d16bf72ca58086
Antti Nilakari <antti.nilakari@corefactory.com>
2019-10-25 11:36:55 +0300

    Rename test variables

--- libuseless.cc
+++ libuseless.cc
@@ -2,10 +2,11 @@
 #include <string>
 
 extern "C" {
-    int meaning_of_life = 42;
+int answer_to_life_the_universe_and_everything = 42;
 
-    int print_and_increment(std::ostream &out, std::string const &str, int increment) {
-        out << str;
-        return increment + 1;
-    }
+int print_and_increment(std::ostream &out, std::string const &str,
+                        int increment) {
+    out << str;
+    return increment + 1;
+}
 }
